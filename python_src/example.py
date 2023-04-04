9e94e3cefb42dc264219058de1c58250c608c1f0
pgerman1 <pgerman1@gmail.com>
2023-04-03 22:20:36 -0400

    added data list

--- python_src/example.py
+++ python_src/example.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 
 import time
+from datetime import datetime
 import os
 import pressure as sensor
 
-POST_READ_DELAY = 0.1
-WRITE_DELAY = 0.05
+POST_READ_DELAY = 0.1   # This is needed with the smbus python library to prevent the unit from reading the i2c bus too quickly. 
+WRITE_DELAY = 0.05      # Update Delay, found this made things smoother.
 
 DATA_FILE = 'pressure_data_output'
+
+#-----------------------------------------------------------
+# Function Main()
+# Main Routine, Create a sensor object, display its atributes,
+# Take Some Data, Write to a File.
+#-----------------------------------------------------------
 
 def main():
     sensor1 = sensor.Pressure()
     display_info(sensor1)
-    read_data(sensor1)
+    acqData = read_data(sensor1)
+    print(acqData)
 #-----------------------------------------------------------
 # Function display_info()
 # Displays the information about the sensor object to the 
@@ -31,16 +39,25 @@ def display_info(sensor):
     os.system("clear")  # Clear Screen and Move On
 
 def read_data(sensor):
-         
+    dataList = [] # empty list to hold data         
     for i in range (0, 240 ):
-        data=sensor.readSample()
-        print(data)
-        time.sleep(.1)
-        pressure_m = sensor.counts2mBar(data)
-        pressure_hg = sensor.mBar2mmhg(pressure_m)
+        counts=sensor.readSample()
+        timeStamp = datetime.now()                  # Grab a Timestamp
+        time.sleep(POST_READ_DELAY)                 # Wait 
+        pressureMbar = sensor.counts2mBar(counts)   # Convert Raw Counts to mBar
+        pressureMmhg = sensor.mBar2mmhg(pressureMbar) # Convert mBar pressure to mmHg
+        
         os.system("clear")
-        print("pressure in mmhg is:" + str(pressure_hg)	+" pressure in mbar is:" + str(pressure_m))	
+        print("-----------------------------------")
+        print("pressure in mmhg is:" + str(counts))
+        print("-----------------------------------")
+        print("pressure in mmhg is:" + str(pressureMmhg))
+        print("-----------------------------------")
+        print(" pressure in mbar is:" + str(pressureMbar))	
+        print("-----------------------------------")
         time.sleep(WRITE_DELAY)
+        dataList.append =[i,timeStamp, counts, pressureMbar, pressureMmhg]     
+    return dataList
 
 
 
--- python_src/pressure.py
+++ python_src/pressure.py
@@ -10,6 +10,10 @@ Member functions of this class
 
 This driver was designed for the Raspberry pi using the
 smbus library for python
+
+This was originally used for testing hardware and can be used in a pinch
+for simple projects or trying out a new sensor before commiting to another
+language.
 """
 
 #Import Modules
@@ -33,8 +37,8 @@ FIFO_DATA_REG = int(0x00)			# Data Register to Read from on at the Address
 DATA_READ_BYTES = 2					# Number of Bytes to read from the I2C Bus (2B, 1 Word)
 
 # Class Definition
-class Pressure ():
 
+class Pressure ():
 	#Base Constructor Method, Default Settings
 
 	def __init__(self) -> None:
@@ -49,27 +53,58 @@ class Pressure ():
 		self.bus = smbus.SMBus(DEFAULT_BUS)
 
 	#******************************************************
+	#Function counts2mbar(counts)
+	#converts ADC Counts to pressure in mBar
+	# Args - Counts
+	# Returns - Pressure in mBar
+	#******************************************************
+	def counts2mBar(self,counts) -> float:
+		pressure=(((counts - self.minCounts) * (self.pressureMax-self.pressureMin)) / (self.maxCounts - self.minCounts)) + self.pressureMin
+		return float(pressure);
+
+	#******************************************************
 	# Function mBar2mmhg()
 	# Converts from millibar pressure to inches of mercury
 	# args - pMbar 
 	#******************************************************
-	def mBar2mmhg(self,pMbar):
+	def mBar2mmhg(self,pMbar) -> float:
 		return (pMbar * MMHG_MBAR_RATIO)
+	
 	#******************************************************
-	#Function readSample()
-	#Reads from 
+	#Function readCounts()
+	#Reads Counts from the I2C Object over the I2C bus. Reads
+	# 2 Bytes from the Data Register, but we perform and endian
+	# Swap on the two data samples with a bit shift and OR.
+
+	# Args - Sensor Object
+	# Returns - ADC Counts
 	#******************************************************
-	def readSample(self):
+	def readCounts(self) -> int:
+		# Read a Raw Data Sample
 		dataSample= self.bus.read_i2c_block_data(
 			self.address, 
 			FIFO_DATA_REG, 
 			DATA_READ_BYTES)
-		adcCounts = (dataSample[0]<<8 )  | (dataSample[1] ) 
+		adcCounts = (dataSample[0]<<8 )  | (dataSample[1] ) 		# Perform Endian Swap	
 		return adcCounts;
 	#******************************************************
-	#Function counts2mbar()
-	#converts ADC Counts to pressure in mBar
+	# Function readPressureMbar()
+	# Reads mBar Pressure from the I2C Object over the I2C 
+	# bus, starts with counts and then converts to mBar
+	# Args - Sensor Object
+	# Returns - Pressure in mBar
 	#******************************************************
-	def counts2mBar(self,counts):
-		pressure=(((counts - self.minCounts) * (self.pressureMax-self.pressureMin)) / (self.maxCounts - self.minCounts)) + self.pressureMin
-		return float(pressure);
+	def readPressureMbar(self) -> float:
+		data = self.readCounts()
+		return self.counts2mBar(data)
+
+	#******************************************************
+	# Function readPressureHg()
+	# Reads mBar Pressure from the I2C Object over the I2C 
+	# bus, and converts to mBar with mBar2mmhg utility function.
+	# Args - Sensor Object
+	# Returns - Pressure in mmHg
+	#******************************************************
+	def readPressureHg(self) -> float:
+		return self.mBar2mmhg( self.readPressureMbar() )
+
