7e4d76999e112d41b55b26be3d63f71b6ee51f43
pgerman1 <pgerman1@gmail.com>
2023-04-03 20:59:52 -0400

    update to check data

--- python_src/example.py
+++ python_src/example.py
@@ -4,12 +4,20 @@ import time
 import os
 import pressure as sensor
 
+POST_READ_DELAY = 0.1
+WRITE_DELAY = 0.05
+
+DATA_FILE = 'pressure_data_output'
+
 def main():
     sensor1 = sensor.Pressure()
     display_info(sensor1)
     read_data(sensor1)
-
-
+#-----------------------------------------------------------
+# Function display_info()
+# Displays the information about the sensor object to the 
+# standard output for debugging.
+#-----------------------------------------------------------
 def display_info(sensor):
 
     print("Sensor Bus :" + str(sensor.bus))
@@ -19,6 +27,8 @@ def display_info(sensor):
     print("Sensor Max Pressure :" + str(sensor.pressureMax))
     print("Sensor Min Pressure :" + str(sensor.pressureMin))
 
+    time.sleep(2)       # 2 Second Display for Splash Screen
+    os.system("clear")  # Clear Screen and Move On
 
 def read_data(sensor):
          
@@ -26,10 +36,12 @@ def read_data(sensor):
         data=sensor.readSample()
         print(data)
         time.sleep(.1)
-    #    pressure_m = sensor.counts2mBar(data)
-    #    pressure_hg = sensor.mBar2mmhg(pressure_m)
+        pressure_m = sensor.counts2mBar(data)
+        pressure_hg = sensor.mBar2mmhg(pressure_m)
         os.system("clear")
     #    print("pressure in mmhg is:" + str(pressure_hg)	+" pressure in mbar is:" + str(pressure_m))	
-        time.sleep(.05)
+        time.sleep(WRITE_DELAY)
+
+
 
 main()
